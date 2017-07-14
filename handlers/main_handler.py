0c3a2203d93cf5ed39271d1fddc80f5fc10e5ce9
JaydaJanae <Jayda.horn@my.hamptonu.edu>
2017-07-14 10:26:23 -0400

    starter page still working on comment box

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -6,9 +6,25 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     	logging.info("MainHandler")
-        html_params = {
-            "title": "Main Title",
+    	# logging.info(users.create_login_url())
+
+
+
+
+    	# author = users.get_current_user()
+
+    	
+
+
+    	# current_user = users.get_current_user()
+
+     #    user_comments_query = Comment.query(Comment.author == current_user.email())
+     #    user_comments = user_comments_query.fetch()
+     	html_params = {
+            "title": "SAVE IT UP",
             "content": "Hello"
+     #        "html_login_url": users.create_login_url("/")
+     #        "html_current_user": current_user,
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
---
+++ resources/money.js
@@ -0,0 +1,3 @@
+$("#btn").on("click", function(){
+  // take what the user has typed in the box
+  var userInput = $("#a")
