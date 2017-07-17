d614ebb7b1d2b2f6215c287eccacaeff66de7a5e
JaydaJanae <Jayda.horn@my.hamptonu.edu>
2017-07-17 10:16:29 -0400

    starting my friends list page
    made new html for friend page

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -2,29 +2,57 @@
 import jinja_env
 import logging
 import webapp2
+from google.appengine.api import users
+
+from google.appengine.ext import ndb
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	logging.info("MainHandler")
-    	# logging.info(users.create_login_url())
+    	logging.info(users.get_current_user())
+    	logging.info(users.create_login_url())
+	
 
 
+	current_user = users.get_current_user()
 
 
-    	# author = users.get_current_user()
-
-    	
-
-
-    	# current_user = users.get_current_user()
-
-     #    user_comments_query = Comment.query(Comment.author == current_user.email())
-     #    user_comments = user_comments_query.fetch()
      	html_params = {
-            "title": "SAVE IT UP",
-            "content": "Hello"
-     #        "html_login_url": users.create_login_url("/")
-     #        "html_current_user": current_user,
+        	"title": "SAVE IT UP",
+            "content": "Hello",
+        	"html_login_url": users.create_login_url("/"),
+            "html_current_user": current_user,
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
+
+
+class Comment(ndb.Model):
+       	author = ndb.StringProperty()
+
+
+	def post(self):
+    		author = user.get_current_user()
+    		if author != None:
+
+			self.redirect("/")
+
+
+
+# class FriendList(webapp2.RequestHandler):
+	# def get(self):
+		
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+	('/', MainHandler),
+	# ('/friendlist', FriendList),
+
+
+
+
+	])
