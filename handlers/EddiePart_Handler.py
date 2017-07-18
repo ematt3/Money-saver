e4922919cfbb0930f604082d54cdab97a3b2c8d9
ematt3 <ematt3@hotmail.com>
2017-07-18 19:19:19 -0400

    more Eddie part edits

--- handlers/EddiePart_Handler.py
+++ handlers/EddiePart_Handler.py
@@ -1,15 +1,22 @@
 import jinja_env
 import logging
 import webapp2
+from google.appengine.api import users
+
+
 
 class EddiePartHandler(webapp2.RequestHandler):
 	def get(self):
 		logging.info("EddiePartHandler")
-		html_params = {
+		current_user = users.get_current_user()
+		if current_user== None:
+			self.redirect("/")
+		else:
+			html_params = {
+	         'email': current_user.email()
+			}
 
-		}
-
-		template = jinja_env.env.get_template('templates/profile.html')
-		self.response.out.write(template.render(html_params))
+			template = jinja_env.env.get_template('templates/profile.html')
+			self.response.out.write(template.render(html_params))
 
 		
