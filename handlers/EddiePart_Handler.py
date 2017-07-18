263218a62e5c8705b5966c594cb2b6330489db24
ACJ123 <flameaj@gmail.com>
2017-07-18 19:19:32 -0400

    h

--- handlers/EddiePart_Handler.py
+++ handlers/EddiePart_Handler.py
@@ -2,11 +2,12 @@ import jinja_env
 import logging
 import webapp2
 
+
 class EddiePartHandler(webapp2.RequestHandler):
 	def get(self):
 		logging.info("EddiePartHandler")
-		html_params = {
 
+		html_params = {
 		}
 
 		template = jinja_env.env.get_template('templates/profile.html')
